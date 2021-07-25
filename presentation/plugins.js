526298f6e31c7c8683d32a9ffc11a8b40139b3ad
Akshay Shankar <sakshays.2000@gmail.com>
2021-07-25 15:39:01 +0530

    initial commit

---
+++ presentation/plugins.js
@@ -0,0 +1,9 @@
+<script src="../reveal.js-plugins/chalkboard/plugin.js"></script>
+<script>
+    Reveal.initialize({
+        // ...
+        plugins: [ RevealChalkboard ],
+        // ...
+    });
+</script>
+
